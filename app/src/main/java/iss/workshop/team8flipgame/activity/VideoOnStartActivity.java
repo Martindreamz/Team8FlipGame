b70806deee904e43ef86f3a5523ec065ba4ea99a
pwinttheingiaungwin <63223245+pwinttheingiaungwin@users.noreply.github.com>
2020-07-17 21:33:42 +0800

    Add video on Start game

---
+++ app/src/main/java/iss/workshop/team8flipgame/activity/VideoOnStartActivity.java
@@ -0,0 +1,66 @@
+package iss.workshop.team8flipgame.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.MediaController;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+import java.util.Calendar;
+
+import iss.workshop.team8flipgame.R;
+
+public class VideoOnStartActivity extends AppCompatActivity {
+
+    protected VideoView videoView;
+    protected MediaController mc;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_on_start);
+
+        //setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_USER_PORTRAIT);
+        setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE
+                        // Set the content to appear under the system bars so that the
+                        // content doesn't resize when the system bars hide and show.
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        // Hide the nav bar and status bar
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+        play();
+    }
+
+    private void play(){
+        videoView = findViewById(R.id.videoView);
+        if (videoView == null)
+            return;
+
+        String path = "android.resource://" + getPackageName() + "/" + R.raw.mov_bbb;
+        Uri uri = Uri.parse(path);
+        videoView.setVideoURI(uri);
+        videoView.start();
+        videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener(){
+
+            public void onCompletion(MediaPlayer arg0) {
+                Intent intent = new Intent(VideoOnStartActivity.this,HomeActivity.class);
+                startActivity(intent);
+            }
+
+        });
+    }
+}
